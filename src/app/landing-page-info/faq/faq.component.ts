1f83b8d4207496fb73f4367b90eecbd6d771e2d1
sakshijain1999 <jains2384@gmail.com>
2021-11-08 19:36:02 +0530

    FAQ Dyanamically  Done --sakshi

---
+++ src/app/landing-page-info/faq/faq.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AfterViewInit, ViewChild } from '@angular/core';
+import Swal from 'sweetalert2';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { LandingPageInfoServiceService } from 'src/app/utils/services/landing-page-info-service.service';
+import { EditFaqComponent } from './edit-faq/edit-faq.component';
+
+
+@Component({
+  selector: 'app-faq',
+  templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.scss']
+})
+export class FAQComponent implements OnInit {
+
+  FAQData: any = [];
+  FaqId: any = [];
+  mode: any;
+  jobTaskData: any = [];
+  ELEMENT_DATA = [];
+  /////////////mat table////////////////
+  displayedColumns: string[] = ['index', 'title', 'edit', 'delete'];
+  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  data: any = [];
+  myId: any;
+  Id: any
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  constructor(private dialog: MatDialog, private landingPageInfo: LandingPageInfoServiceService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllPortal();
+
+  }
+  getAllPortal() {
+
+    this.landingPageInfo.getAllPortal().subscribe((res: any) => {
+      console.log('portalDetails=>', res);
+      let faqData = res.data;
+      faqData.forEach((element, index) => {
+        element.index = index + 1; //adding index
+      });
+
+      this.ELEMENT_DATA = faqData;
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+
+    });
+
+  }
+
+  edit(element) {
+
+    const dialogRef = this.dialog.open(EditFaqComponent, {
+      width: "550px",
+      data: element,
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if ((result == "true")) {
+        this.getAllPortal();
+      }
+      console.log("The dialog was closed");
+    });
+  }
+  delete(item) {
+    console.log("delete", item);
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: `Do you want to delete "${item.title}"?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#00B96F',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Delete!',
+    }).then((result) => {
+      if (result.value) {
+        this.landingPageInfo
+          .deletePortal(item._id)
+          .subscribe((res) => {
+            Swal.fire({
+              title: 'Deleted successfully',
+              showConfirmButton: false,
+              timer: 1000,
+            }); console.log('deleted res', res);
+            this.getAllPortal();
+
+          });
+      }
+    });
+  }
+
+}
+
---
+++ src/app/landing-page-info/faq/ques-ans/edit-question/edit-question.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { LandingPageInfoServiceService } from 'src/app/utils/services/landing-page-info-service.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-edit-question',
+  templateUrl: './edit-question.component.html',
+  styleUrls: ['./edit-question.component.scss']
+})
+export class EditQuestionComponent implements OnInit {
+  editTitle: FormGroup;
+  dataRec: any;
+  FAQ=[];
+  portalData:any = [];
+  
+  constructor(
+    private fb: FormBuilder,
+    private landingPageInfo: LandingPageInfoServiceService,
+    public dialogRef: MatDialogRef<EditQuestionComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.dataRec = data;
+  }
+
+  ngOnInit(): void {
+    console.log(this.dataRec);
+    
+    this.editTitle = this.fb.group({
+      question: [this.dataRec.question, Validators.required],
+      answer: [this.dataRec.answer, Validators.required],
+      portalId: [this.dataRec.portalId, Validators.required],
+    });
+   // this.getAllFaq();
+    this.getAllPortal();
+  }
+  getAllFaq() {
+    this.landingPageInfo.getAllFaq().subscribe((res) => {
+      console.log('getaLlFAQ=>', res);
+      this.FAQ = res.data;
+ 
+    });
+ 
+  }
+  onFormSubmit() {
+
+    let data={
+      question :this.editTitle.get('question').value,
+      answer:this.editTitle.get('answer').value,
+      portalId:this.editTitle.get('portalId').value,
+    
+    }
+    console.log("dataaa",data);
+    
+    this.landingPageInfo
+      .editFAQ(data, this.dataRec._id)
+      .subscribe((resData) => {
+        console.log('editFAQ', resData);
+
+        this.dialogRef.close('true');
+        Swal.fire('Question Edited successfully');
+      });
+  }
+  closeDialog(){
+    this.dialogRef.close('false');
+
+  }
+  getAllPortal(){
+    this.landingPageInfo.getAllPortal().subscribe((res)=>{
+      console.log('getAllPortal=>', res);
+     this.portalData=res.data;
+     console.log('portalData=>', this.portalData);
+    
+   
+    })
+  }
+}
