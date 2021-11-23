2aab1f7e8570c7474fa5360f48e91212d9ecd04f
sakshijain1999 <jains2384@gmail.com>
2021-11-23 19:46:18 +0530

    EditCustomer --sakshi

--- src/app/site-info/add-customer/edit-cust/edit-cust.component.ts
+++ src/app/site-info/add-customer/edit-cust/edit-cust.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup,FormArray } from '@angular/forms';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { LogicalFormInfoService } from 'src/app/utils/services/logical-form-info.service';
@@ -9,7 +9,8 @@ import Swal from 'sweetalert2';
   styleUrls: ['./edit-cust.component.scss']
 })
 export class EditCustComponent implements OnInit {
-  editCustomerForm:FormGroup
+  editCustomerForm:FormGroup;
+  StatesData:any=[''];
   constructor(
     private dialogRef: MatDialogRef<EditCustComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: any,
@@ -23,10 +24,23 @@ export class EditCustComponent implements OnInit {
     this.editCustomerForm = this.fb.group({
       customerName:[this.data.customerName],
       customerContact:[this.data.customerContact],
-      customerContactPhone:[this.data.customerContactPhone],
-      customerEmail:[this.data.customerEmail],
+     // customerContactPhone:[this.data.customerContactPhone],
+      //customerEmail:[this.data.customerEmail],
+      streetAddress:[this.data.streetAddress],
+      stateId:[this.data.stateId._id],
+      postCode:[this.data.postCode],
+      ABN:[this.data.ABN],
+     contacts: this.fb.array([]),
+      position:[this.data.contacts.position],
+    email:[this.data.contacts.email],
+    phone:[this.data.contacts.phone]
+
+
     })
+    this.addCustomerDetails();
+    this.getAllStates();
   }
+
   onSubmit(){
     
     this.logicalFormInfoService.updateCustomer(this.data._id,this.editCustomerForm.value).subscribe(res => {
@@ -41,5 +55,33 @@ export class EditCustComponent implements OnInit {
   }
   close() {
     this.dialogRef.close();
+}
+addCustomerDetails() {
+  this.customerArr().push(this.customerForm());
+
+}
+customerArr(): FormArray {
+  return this.editCustomerForm.get('contacts') as FormArray;
+}
+customerForm(): FormGroup {
+  return this.fb.group({
+    // position:[this.data.contacts.position],
+    // email:[this.data.contacts.email],
+    // phone:[this.data.contacts.phone]
+
+  });
+}
+removeCustomerDetails(i) {
+  const item = <FormArray>this.editCustomerForm.controls['contacts'];
+  if (item.length > 1) {
+    item.removeAt(i);
+
+  }
+}
+getAllStates() {
+  this.logicalFormInfoService.getAllStates().subscribe((res: any) => {
+    console.log('setStatesDetails=>', res);
+    this.StatesData = res.data;
+  });
 }
 }
