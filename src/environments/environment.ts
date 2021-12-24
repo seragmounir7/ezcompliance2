a9e11dcff1d37dcfce72a55541034722d1f00c24
Abhijitkachhawa <abhijitkachhawa@gmail.com>
2021-12-24 15:35:27 +0530

    change in resk-assesment-table,site-inpection,incident-report page,  Enviroment.ts   _abhijit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://13.127.90.176:3148/api/v1/',
-   stagingUrl:'http://54.201.160.69:3152/',
+   stagingUrl:'http://localhost:4200/',
   allRouteTemp:true
 
 
