476e6f0fc918dabcba63e80e08e479fb51ce4317
Ajinkya <ajinkyajacob@gmail.com>
2022-05-05 15:19:28 +0530

    Updating staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
 
-	apiUrl: false
+	apiUrl: true
 		? 'http://13.127.90.176:3148/api/v1/'
 		: 'http://localhost:3148/api/v1/' /* 'http://54.201.160.69:3148/api/v1/' */,
 	stagingUrl: 'http://localhost:4200/',
